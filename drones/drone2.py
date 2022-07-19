7d646dd419ddf36ae3fa59c5884252800503416d
Filippo Casadei <filippo.casadei9@studio.unibo.it>
2022-07-19 10:43:46 +0200

    Added command check when the drones receive an address to deliver to

--- drones/drone2.py
+++ drones/drone2.py
@@ -9,11 +9,9 @@ s.settimeout(5)
 ip = "192.168.1.10" #IP address of this drone
 gateway_port = 25000 #Port used by the gateway
 
-gateway_address = ('localhost',gateway_port) 
-init_message = "Ready to serve..."
-completion_message = "Delivery successful"
+gateway_address = ('localhost',gateway_port)
 
-message = ip + ":register:" + init_message
+message = ip + ":register"
 sent = s.sendto(message.encode(), gateway_address)
 
 try:
@@ -21,17 +19,26 @@ try:
     # Verifying that the gateway received the message
     # and waiting for a response
     if response == "OK":
+        print("Ready to serve...")
         s.settimeout(0)
         while True:
             # Getting the delivery address from the gateway 
             data, gateway = s.recvfrom(4096)
-            print("Delivering to: %s" % data.decode('utf8'))
-            # The drone takes some time to deliver the package
-            time.sleep(random.randint(1, 5)) 
-            message = ip + ":delivered:" + completion_message
-            # Informing the gateway that the delivery was successful
-            # and the drone is ready for the next delivery
-            s.sendto(message.encode() ,gateway_address)
+            payload = data[0].decode('utf-8').split(":")
+            command = payload[0]
+            if command == "deliver":
+                address = payload[1]
+                s.sendto("OK".encode(),gateway_address)
+                print("Delivering to: %s" % address)
+                # The drone takes some time to deliver the package
+                time.sleep(random.randint(1, 5)) 
+                message = ip + ":delivered" 
+                # Informing the gateway that the delivery was successful
+                # and the drone is ready for the next delivery
+                s.sendto(message.encode() ,gateway_address)
+                print("Delivery successful")
+            else:
+                print("Unknown command: %s" % command)
     else:
         # The received message didn't match expected message
         print("Connection failed, shutting down")
