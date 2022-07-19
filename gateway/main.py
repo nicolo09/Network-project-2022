e1702d0afdb31a97881754355cc230dee80377a9
Nicolo Guerra <nicolo.guerra@studio.unibo.it>
2022-07-19 12:15:32 +0200

    Fixed drone delivering dispatching

--- gateway/main.py
+++ gateway/main.py
@@ -4,21 +4,23 @@ from socket import *
 import threading
 
 connected_drones = {}
-deliveries_to_do = []
+deliveries_to_do = {}
 deliveries_in_progress = {}
 bufferSize = 1024
 localhost = "127.0.0.1"
-timeout = 3
+timeoutTime = 3
 clientConnectionSocket = None
+lock = threading.Lock()
 
 def start_deliver(drone, address):
     if deliveries_in_progress.get(drone) is None:
+        #drone = (localhost, connected_drones[drone])
         print("Telling drone {drone} to deliver to {address}".format(drone=drone, address=address))
-        droneSocket.sendto("deliver:{address}".format(address=address), (localhost, drone[1]))
+        droneSocket.sendto(("deliver:{address}".format(address=address)).encode(), (localhost, connected_drones[drone]))
         try:
             message = ""
             while message != "OK":
-                droneSocket.settimeout(timeout)
+                droneSocket.settimeout(timeoutTime)
                 bytesAddressPair, realAddress = droneSocket.recvfrom(bufferSize)
                 # Get the drone's address and port from message
                 payload = bytesAddressPair.decode("utf-8").split(":")
@@ -33,39 +35,50 @@ def start_deliver(drone, address):
             tell_client("Failed due to {drone} timeout".format(drone=drone))
     
 def wait_for_drone(droneSocket):
+    print("Thread waiting for a drone to send messages on {this_ip}:{drone_port}".format(
+        this_ip=gethostbyname(gethostname()), drone_port=dronePort))
     while True:
-        print("Thread waiting for a drone to register as free on {this_ip}:{drone_port}".format(
-            this_ip=gethostbyname(gethostname()), drone_port=dronePort))
-        bytesAddressPair, realAddress = droneSocket.recvfrom(bufferSize)
-        # Get the drone's address and port from message
-        payload = bytesAddressPair.decode("utf-8").split(":")
-        addressPort = (payload[0], realAddress[1])
-        message = (payload[1])
+        droneSocket.settimeout(timeoutTime)
+        try:
+            bytesAddressPair, realAddress = droneSocket.recvfrom(bufferSize)
+            # Get the drone's address and port from message
+            payload = bytesAddressPair.decode("utf-8").split(":")
+            addressPort = (payload[0], realAddress[1])
+            message = (payload[1])
 
-        print("Message from drone:{}".format(message))
-        print("Drone IP Address:{}".format(addressPort))
+            print("Message from drone:{}".format(message))
+            print("Drone IP Address:{}".format(addressPort))
 
-        if (message == "register"):
-            print("Drone registered as free on {address}".format(
-                address=addressPort[0], port=addressPort[1]))
-            connected_drones[addressPort[0]](addressPort[1])
-            droneSocket.sendto(b"OK", realAddress)
-        elif (message == "unregister"):
-            print("Drone unregistered on {address}".format(
-                address=addressPort[0], port=addressPort[1]))
-            connected_drones.pop(addressPort)
-            droneSocket.sendto(b"OK", realAddress)
-        elif (message == "delivered"):
-            print("Drone {drone} delivered to {delivery_address}. Now its free.".format(
-                drone=addressPort, delivery_address=deliveries_in_progress[addressPort]))
-            deliveries_in_progress.pop(addressPort)
-            droneSocket.sendto(b"OK", realAddress)
-        # elif (message == "failed"):
-        #     print("Drone {drone} failed to deliver to {delivery_address}. Now its free.".format(
-        #         drone=addressPort, delivery_address=deliveries_in_progress[addressPort]))
-        #     deliveries_to_do.append(deliveries_in_progress.pop(addressPort))
-        print(connected_drones)
-
+            if (message == "register"):
+                print("Drone registered as free on {address}".format(
+                    address=addressPort[0], port=addressPort[1]))
+                connected_drones[addressPort[0]] = addressPort[1]
+                droneSocket.sendto(b"OK", realAddress)
+            elif (message == "unregister"):
+                print("Drone unregistered on {address}".format(
+                    address=addressPort[0], port=addressPort[1]))
+                connected_drones.pop(addressPort)
+                droneSocket.sendto(b"OK", realAddress)
+            elif (message == "delivered"):
+                print("Drone {drone} delivered to {delivery_address}. Now its free.".format(
+                    drone=addressPort, delivery_address=deliveries_in_progress[addressPort[0]]))
+                deliveries_in_progress.pop(addressPort[0])
+                droneSocket.sendto(b"OK", realAddress)
+            # elif (message == "failed"):
+            #     print("Drone {drone} failed to deliver to {delivery_address}. Now its free.".format(
+            #         drone=addressPort, delivery_address=deliveries_in_progress[addressPort]))
+            #     deliveries_to_do.append(deliveries_in_progress.pop(addressPort))
+            else:
+                print("Unknown message from drone: " + message)
+            print(connected_drones)
+        except timeout:
+            #No messages received from drones in 1 second, continue checking deliveries to do
+            with lock:    
+                if deliveries_to_do:
+                    print("Dispatching deliveries...")
+                    for delivery in deliveries_to_do:
+                        start_deliver(delivery, deliveries_to_do[delivery])
+                    deliveries_to_do.clear()
 
 def wait_for_client(clientSocket):
     while True:
@@ -89,6 +102,7 @@ def wait_for_client(clientSocket):
                 address=addressPort[0], port=addressPort[1]))
             connectedClient = addressPort
             try:
+                #Message handling
                 while True:
                     bytesAddressPair = clientConnectionSocket.recv(bufferSize)
                     # Get the client's address and port from message
@@ -103,7 +117,11 @@ def wait_for_client(clientSocket):
                         address = data[1]
                         print("Client {client} wants {drone} to deliver to {delivery_address}".format(
                             client=addressPort, drone=drone, delivery_address=address))
-                        start_deliver(drone, payload[3])
+                        with lock:
+                            if drone not in deliveries_to_do:
+                                deliveries_to_do[drone] = address
+                            else:
+                                tell_client("Failed due to {drone} already busy".format(drone=drone))
                     elif (command == "drones"):
                         print("Client {client} wants to know all available drones".format(
                             client=addressPort))
