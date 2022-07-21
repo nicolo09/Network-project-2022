23a69196dc06b7c16c217a69a02b92c6cbb177a4
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-07-21 15:00:38 +0200

    Added command to recive list of available drones

--- client/client.py
+++ client/client.py
@@ -8,6 +8,7 @@ my_port=17000
 gateway_address = '127.0.0.1'
 gateway_port = 18000
 BUFFER=256
+d="DRONES"
 
 
 def client_waits(s):
@@ -22,25 +23,30 @@ def client_waits(s):
 
 def client_input(s):
     while True:
-        address=input("C:Insert an address to use in the delivery ")
+        address=input("C:Insert an address to use in the delivery (insert "+d+" to request a list of available drones) ")
         if len(address)>0:
             if address.lower()==e.lower():
                 print("C:Closing Client...")
                 s.close()
                 sys.exit()
             else:
-                iden=input("C:You can choose the drone to carry out your delivery. Insert IPv4 or Drone Identifier (insert -1 to go back to address input)")
-                if len(iden)>0:
-                    if iden.lower()==e.lower():
-                        print("C:Closing Client...")
-                        s.close()
-                        sys.exit()
-                        
-                    else:
-                        if iden!=str(-1):
-                            print("C:Contacting the gateway about your delivery...")
-                            message = my_host+":"+"deliver:"+iden+":"+address
-                            s.sendall(message.encode());
+                if address.lower()==d.lower():
+                    print("C:Asking the gateway about available drones...")
+                    message = my_host+":"+"drones:"
+                    s.sendall(message.encode());
+                else:
+            
+                    iden=input("C:You can choose the drone to carry out your delivery. Insert IPv4 or Drone Identifier (insert -1 to go back to address input) ")
+                    if len(iden)>0:
+                        if iden.lower()==e.lower():
+                            print("C:Closing Client...")
+                            s.close()
+                            sys.exit()  
+                        else:
+                            if iden!=str(-1):
+                                print("C:Contacting the gateway about your delivery...")
+                                message = my_host+":"+"deliver:"+iden+":"+address
+                                s.sendall(message.encode());
                             
                         
         else:
