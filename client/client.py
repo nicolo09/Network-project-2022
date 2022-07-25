4f7a5a74520a72412bbd6b6b7673c92c0045125e
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2022-07-25 17:15:56 +0200

    Added command ping to Client

--- client/client.py
+++ client/client.py
@@ -1,27 +1,41 @@
 import socket
 import sys
-from threading import Thread
+from threading import Thread, Lock
 import threading
+import time
 
 e="EXIT"
+d="DRONES"
+p='PING'
 my_host='10.10.10.1'
 my_port=17000
 gateway_address = '127.0.0.1'
 gateway_port = 26000
 BUFFER=256
-d="DRONES"
+MILLISECONDS=1000
+ping_ans='pong'
+time_waiting=0
+
 terminate_event = threading.Event()
 
-
-def client_waits(s):
+def client_waits(s,lock):
     try:
         with s as s:
             while True:
                 message=s.recv(BUFFER)
                 message=message.decode()
+                a=time.time()
                 if len(message)>0:
-                    print('Received message:')
-                    print(message)
+                    if message.lower()==ping_ans.lower(): 
+                        lock.acquire()
+                        #reading the time 
+                        global time_waiting
+                        te=((a-time_waiting)/2)*MILLISECONDS
+                        lock.release()
+                        print('Time elapsed to send message: '+str(te)+' milliseconds')
+                    else:
+                        print('Received message:')
+                        print(message)
                 else:
                     print('Gateway has disconnected, ending client')
                     terminate_event.set()
@@ -31,11 +45,11 @@ def client_waits(s):
         return
     
 
-def client_input(s):
+def client_input(s,lock):
     try:
         while True:
             try:
-                address=input("Insert an address to use in the delivery (insert "+d+" to request a list of available drones)\n")
+                address=input("Insert an address to use in the delivery (insert "+d+" to request a list of available drones, or "+p+" to ping the gateway)\n")
                 if len(address)>0:
                     if address.lower()==e.lower():
                         print("Closing Client...")
@@ -47,17 +61,27 @@ def client_input(s):
                             message = my_host+":"+"drones:"
                             s.sendall(message.encode())
                         else:
-                            iden=input("You can choose the drone to carry out your delivery. Insert IPv4 or (insert -1 to go back to address input)\n")
-                            if len(iden)>0:
-                                if iden.lower()==e.lower():
-                                    print("Closing Client...")
-                                    terminate_event.set()
-                                    return
-                                else:
-                                    if iden!=str(-1):
-                                        print("Contacting the gateway about your delivery...")
-                                        message = my_host+":"+"deliver:"+iden+":"+address
-                                        s.sendall(message.encode())
+                            if address.lower()==p.lower():
+                                #Ask for the ping
+                                print('Asking the ping...')
+                                message=my_host+":"+"ping:"
+                                global time_waiting
+                                lock.acquire()
+                                time_waiting=time.time()
+                                lock.release()
+                                s.sendall(message.encode())
+                            else:
+                                iden=input("You can choose the drone to carry out your delivery. Insert IPv4 or (insert -1 to go back to address input)\n")
+                                if len(iden)>0:
+                                    if iden.lower()==e.lower():
+                                        print("Closing Client...")
+                                        terminate_event.set()
+                                        return
+                                    else:
+                                        if iden!=str(-1):
+                                            print("Contacting the gateway about your delivery...")
+                                            message = my_host+":"+"deliver:"+iden+":"+address
+                                            s.sendall(message.encode())
                 else:
                     print("Address is wrong, try again")
             except (KeyboardInterrupt, EOFError, SystemExit):
@@ -84,8 +108,10 @@ if __name__=='__main__':
         print("Couldn't connect, retry later")
         sys.exit(0)
 
-    co=Thread(target=client_waits,args=(s,))
-    ci=Thread(target=client_input,args=(s,))
+    lock=Lock()
+    co=Thread(target=client_waits,args=(s,lock))
+    ci=Thread(target=client_input,args=(s,lock))
+
     try:
         ci.setDaemon(True)
         ci.start()
